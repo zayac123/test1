40ef7254e80a7da101844e104efcfb6e42a16acd
zayac123 <37961187+zayac123@users.noreply.github.com>
2025-03-15 13:36:39 +0200

    Add files via upload

---
+++ startqadmin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# BAT / CMD goto function
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+pkill -f cpuminer
+sleep 0.2s;
+/home/admin/cpuminer-multi/cpuminer -a yescryptR16 -o stratum+tcp://eu.hulkhash.uk:9966 -u qPavScognuackDL5hjFnpf6sEQKdGCKg3q -p c=QOGE
+
